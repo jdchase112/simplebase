8092ab6e1a543e442095e5a1313a9d39ccf8fdcb
jdchase112 <jdchase@hawaii.edu>
2026-09-22 15:44:16 -1000

    Enhance site navigation: implement mobile menu button, update header styles, and improve accessibility features

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log("script.js is loaded");
+
+const siteNav = document.querySelector('.site-nav');
+const menuButton = document.querySelector('.menu-button');
+
+menuButton.onclick = () => {
+    if (siteNav.getAttribute('data-navstate') === 'open') {
+        siteNav.setAttribute('data-navstate', 'closed')
+    } else {
+        siteNav.setAttribute('data-navstate', 'open')
+    };
+}
